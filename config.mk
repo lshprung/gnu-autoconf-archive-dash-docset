75a9d380fdeb015e09a93e875a9a9edca9f16344
Louie S <louie@example.com>
2024-06-20 16:56:29 -0400

    Update Makefile

--- config.mk
+++ config.mk
@@ -1,35 +1,10 @@
-DOCSET_NAME = GNU_Autoconf_Archive
+SRC_ICON_FILE=$(SOURCE_DIR)/icon.png
 
-DOCSET_DIR    = $(DOCSET_NAME).docset
-CONTENTS_DIR  = $(DOCSET_DIR)/Contents
-RESOURCES_DIR = $(CONTENTS_DIR)/Resources
-DOCUMENTS_DIR = $(RESOURCES_DIR)/Documents
-
-INFO_PLIST_FILE = $(CONTENTS_DIR)/Info.plist
-INDEX_FILE      = $(RESOURCES_DIR)/docSet.dsidx
-ICON_FILE       = $(DOCSET_DIR)/icon.png
-ARCHIVE_FILE    = $(DOCSET_NAME).tgz
-
-MANUAL_VERSION = 2023.02.20
-MANUAL_URL  = https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-$(MANUAL_VERSION).tar.xz
-MANUAL_SRC = tmp/autoconf-archive-$(MANUAL_VERSION)
+VERSION = 2023.02.20
+MANUAL_URL  = https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-$(VERSION).tar.xz
+MANUAL_SRC = tmp/autoconf-archive-$(VERSION)
 MANUAL_SRC_MAKEFILE = $(MANUAL_SRC)/Makefile
 MANUAL_FILE = $(MANUAL_SRC)/doc/autoconf-archive.html
-
-DOCSET = $(INFO_PLIST_FILE) $(INDEX_FILE) $(ICON_FILE)
-
-all: $(DOCSET)
-
-archive: $(ARCHIVE_FILE)
-
-clean:
-	rm -rf $(DOCSET_DIR) $(ARCHIVE_FILE)
-
-tmp:
-	mkdir -p $@
-
-$(ARCHIVE_FILE): $(DOCSET)
-	tar --exclude='.DS_Store' -czf $@ $(DOCSET_DIR)
 
 $(MANUAL_SRC): tmp
 	curl -o $@.tar.xz $(MANUAL_URL)
@@ -41,25 +16,10 @@ $(MANUAL_SRC_MAKEFILE): $(MANUAL_SRC)
 $(MANUAL_FILE): $(MANUAL_SRC_MAKEFILE)
 	cd $(MANUAL_SRC) && make html
 
-$(DOCSET_DIR):
-	mkdir -p $@
-
-$(CONTENTS_DIR): $(DOCSET_DIR)
-	mkdir -p $@
-
-$(RESOURCES_DIR): $(CONTENTS_DIR)
-	mkdir -p $@
-
 $(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
 	mkdir -p $@
 	cp -r $(MANUAL_FILE)/* $@
 
-$(INFO_PLIST_FILE): src/Info.plist $(CONTENTS_DIR)
-	cp src/Info.plist $@
-
-$(INDEX_FILE): src/index.sh $(DOCUMENTS_DIR)
+$(INDEX_FILE): $(SOURCE_DIR)/src/index.sh $(DOCUMENTS_DIR)
 	rm -f $@
-	src/index.sh $@ $(DOCUMENTS_DIR)/*.html
-
-$(ICON_FILE): src/icon.png $(DOCSET_DIR)
-	cp src/icon.png $@
+	$(SOURCE_DIR)/src/index.sh $@ $(DOCUMENTS_DIR)/*.html
--- src/index.sh
+++ src/index.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
-create_table() {
-	sqlite3 "$DB_PATH" "CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);"
-	sqlite3 "$DB_PATH" "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);"
-}
+# shellcheck source=../../../scripts/create_table.sh
+. "$(dirname "$0")"/../../../scripts/create_table.sh
+# shellcheck source=../../../scripts/insert.sh
+. "$(dirname "$0")"/../../../scripts/insert.sh
+
+DB_PATH="$1"
+shift
 
 get_title() {
 	FILE="$1"
@@ -22,14 +25,6 @@ get_type() {
 	fi
 }
 
-insert() {
-	NAME="$1"
-	TYPE="$2"
-	PAGE_PATH="$3"
-
-	sqlite3 "$DB_PATH" "INSERT INTO searchIndex(name, type, path) VALUES (\"$NAME\",\"$TYPE\",\"$PAGE_PATH\");"
-}
-
 insert_pages() {
 	# Get title and insert into table for each html file
 	while [ -n "$1" ]; do
@@ -43,14 +38,11 @@ insert_pages() {
 				PAGE_TYPE="Guide"
 			fi
 			#echo "$PAGE_TYPE"
-			insert "$PAGE_NAME" "$PAGE_TYPE" "$(basename "$1")"
+			insert "$DB_PATH" "$PAGE_NAME" "$PAGE_TYPE" "$(basename "$1")"
 		fi
 		shift
 	done
 }
 
-DB_PATH="$1"
-shift
-
-create_table
+create_table "$DB_PATH"
 insert_pages "$@"
